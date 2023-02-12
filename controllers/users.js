70cfa8d3405fd14e4f98fa72f5329060b4976506
prajwal080501 <praju.ladkat@gmail.com>
2023-02-12 14:13:03 +0530

    fixed issues with update user api function

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,24 @@
+import User from "../models/User.js";
+
 export const updateUser = async (req, res) => {
-    if(req.body.userId === req.params.id || req.user.isAdmin){
-        if(req.body.password){
-            try{
-                const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(req.body.password, salt);
-            }catch(err){
-                return res.status(500).json(err);
-            }
-        }
-        try{
-            const user = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body,
-            });
-            res.status(200).json("Account has been updated");
-        }catch(err){
-            return res.status(500).json(err);
-        }
-    }else{
-        res.status(403).json({
+    const user = User.findById(req.params.id).select("-_");
+    if (!user) {
+        res.status(404).json({
             "status": "error",
-            "code": 403,
-            "message": "You can update only your account",
+            "code": 404,
+            "message": "User not found",
             "data": "null"
         })
+    }
+    try {
+        if(req.body.password) {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error);
     }
 };
