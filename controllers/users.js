ed7c717fae6bba1dab932b1700745d76ad839373
prajwal080501 <praju.ladkat@gmail.com>
2023-02-12 14:53:31 +0530

    added api functions for following and unfollowing a user

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 
 export const updateUser = async (req, res) => {
+   if(req.body.userId === req.params.id || req.body.isAdmin) {
     const user = User.findById(req.params.id).select("-_");
     if (!user) {
         res.status(404).json({
@@ -21,4 +22,142 @@ export const updateUser = async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
+    }
+    else {
+        res.status(403).json({
+            "status": "error",
+            "code": 403,
+            "message": "You can only update your account",
+            "data": "null"
+        })
+    }
 };
+
+export const deleteUser = async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        const user = await User.findById(req.params.id).select("-_");
+    if (!user) {
+        res.status(404).json({
+            "status": "error",
+            "code": 404,
+            "message": "User not found",
+            "data": "null"
+        })
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            "status": "success",
+            "code": 200,
+            "message": "User deleted successfully",
+            "data": user
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    }
+    else {
+        res.status(403).json({
+            "status": "error",
+            "code": 403,
+            "message": "You can only delete your account",
+            "data": "null"
+        })
+    }
+};
+
+export const getUser = async (req, res) => {
+    const user = await User.findById(req.params.id).select("-_");
+    if (!user) {
+        res.status(404).json({
+            "status": "error",
+            "code": 404,
+            "message": "User not found",
+            "data": "null"
+        })
+    }
+    try {
+        const { password, updatedAt, ...other } = user._doc; // remove password and updatedAt from the response
+        res.status(200).json(other);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+export const followUser = async (req, res) => {
+    if(req.body.userId === req.params.id){
+        res.status(403).json({
+            "status": "error",
+            "code": 403,
+            "message": "You can't follow yourself",
+            "data": "null"
+        })
+    }
+    else{
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
+                res.status(200).json({
+                    "status": "success",
+                    "code": 200,
+                    "message": "User has been followed",
+                    "data": "null"
+                });
+            }
+            else{
+                res.status(403).json({
+                    "status": "error",
+                    "code": 403,
+                    "message": "You already follow this user",
+                    "data": "null"
+                })
+            }
+        }
+        catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
+
+export const unFollowUser = async (req, res) => {
+    if(req.body.userId === req.params.id){
+        res.status(403).json({
+            "status": "error",
+            "code": 403,
+            "message": "You can't follow yourself",
+            "data": "null"
+        })
+    }
+    else{
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json({
+                    "status": "success",
+                    "code": 200,
+                    "message": "User has been unflollowed",
+                    "data": "null"
+                });
+            }
+            else{
+                res.status(403).json({
+                    "status": "error",
+                    "code": 403,
+                    "message": "You don't follow this user",
+                    "data": "null"
+                })
+            }
+        }
+        catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
+    
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { updateUser } from "../controllers/users.js";
+import { deleteUser, followUser, getUser, updateUser } from "../controllers/users.js";
 
 const router = express.Router();
 
 // update user
 router.put("/:id", updateUser);
 // delete user
+router.delete("/:id", deleteUser);
 // get a user
+router.get("/:id", getUser);
 // follow a user
+router.put("/:id/follow", followUser);
 // unfollow a user
 
 
